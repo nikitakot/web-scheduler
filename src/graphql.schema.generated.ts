bce1025f446dd2f883126dea715d6d9fedd0d253
Nikita Kot <kotn01@outlook.cz>
2020-06-17 19:41:32 +0200

    fix unit tests

--- src/graphql.schema.generated.ts
+++ src/graphql.schema.generated.ts
@@ -1,4 +1,3 @@
-
 /** ------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
  * -------------------------------------------------------
@@ -7,82 +6,94 @@
 /* tslint:disable */
 /* eslint-disable */
 export class LoginInput {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export class MonitoredEndpointInput {
-    name: string;
-    url: string;
-    monitoredInterval: number;
-    disabled?: boolean;
+  name: string;
+  url: string;
+  monitoredInterval: number;
+  disabled?: boolean;
 }
 
 export class SignUpInput {
-    username: string;
-    email: string;
-    password: string;
+  username: string;
+  email: string;
+  password: string;
 }
 
 export class UpdateMonitoredEndpointInput {
-    id: string;
-    name?: string;
-    url?: string;
-    monitoredInterval?: number;
-    disabled?: boolean;
+  id: string;
+  name?: string;
+  url?: string;
+  monitoredInterval?: number;
+  disabled?: boolean;
 }
 
 export class AuthPayload {
-    id: string;
-    username: string;
-    email: string;
-    jwt: string;
+  id: string;
+  username: string;
+  email: string;
+  jwt: string;
 }
 
 export class MonitoredEndpoint {
-    id: string;
-    name: string;
-    url: string;
-    monitoredInterval: number;
-    disabled: boolean;
-    owner: User;
-    monitoringResult: MonitoringResult[];
-    createdAt: DateTime;
-    updatedAt: DateTime;
-    checkedAt?: DateTime;
+  id: string;
+  name: string;
+  url: string;
+  monitoredInterval: number;
+  disabled: boolean;
+  owner: User;
+  monitoringResult: MonitoringResult[];
+  createdAt: DateTime;
+  updatedAt: DateTime;
+  checkedAt?: DateTime;
 }
 
 export class MonitoringResult {
-    id: string;
-    statusCode: number;
-    payload: string;
-    monitoredEndpoint: MonitoredEndpoint;
-    createdAt: DateTime;
-    updatedAt: DateTime;
+  id: string;
+  statusCode: number;
+  payload: string;
+  monitoredEndpoint: MonitoredEndpoint;
+  createdAt: DateTime;
+  updatedAt: DateTime;
 }
 
 export abstract class IMutation {
-    abstract signup(signUpInput?: SignUpInput): AuthPayload | Promise<AuthPayload>;
+  abstract signup(
+    signUpInput?: SignUpInput,
+  ): AuthPayload | Promise<AuthPayload>;
 
-    abstract login(loginInput?: LoginInput): AuthPayload | Promise<AuthPayload>;
+  abstract login(loginInput?: LoginInput): AuthPayload | Promise<AuthPayload>;
 
-    abstract createMonitoredEndpoint(monitoredEndpointInput?: MonitoredEndpointInput): MonitoredEndpoint | Promise<MonitoredEndpoint>;
+  abstract createMonitoredEndpoint(
+    monitoredEndpointInput?: MonitoredEndpointInput,
+  ): MonitoredEndpoint | Promise<MonitoredEndpoint>;
 
-    abstract updateMonitoredEndpoint(updateMonitoredEndpointInput?: UpdateMonitoredEndpointInput): MonitoredEndpoint | Promise<MonitoredEndpoint>;
+  abstract updateMonitoredEndpoint(
+    updateMonitoredEndpointInput?: UpdateMonitoredEndpointInput,
+  ): MonitoredEndpoint | Promise<MonitoredEndpoint>;
 }
 
 export abstract class IQuery {
-    abstract monitoredEndpoint(id: string): MonitoredEndpoint | Promise<MonitoredEndpoint>;
+  abstract monitoredEndpoint(
+    id: string,
+  ): MonitoredEndpoint | Promise<MonitoredEndpoint>;
 
-    abstract monitoredEndpoints(): MonitoredEndpoint[] | Promise<MonitoredEndpoint[]>;
+  abstract monitoredEndpoints():
+    | MonitoredEndpoint[]
+    | Promise<MonitoredEndpoint[]>;
 
-    abstract findMonitoredEndpoints(url: string): MonitoredEndpoint[] | Promise<MonitoredEndpoint[]>;
+  abstract findMonitoredEndpoints(
+    url: string,
+  ): MonitoredEndpoint[] | Promise<MonitoredEndpoint[]>;
 }
 
 export class User {
-    id: string;
-    username: string;
-    monitoredEndpoint: MonitoredEndpoint[];
+  id: string;
+  username: string;
+  monitoredEndpoint: MonitoredEndpoint[];
 }
 
 export type DateTime = any;
--- src/monitored-endpoint/monitored-endpoint.resolver.spec.ts
+++ src/monitored-endpoint/monitored-endpoint.resolver.spec.ts
@@ -1,18 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MonitoredEndpointResolver } from './monitored-endpoint.resolver';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MonitoredEndpointEntity } from './monitored-endpoint.entity';
+import { Repository } from 'typeorm';
+import { MonitoringResultEntity } from '../monitoring-result/monitoring-result.entity';
+import { TasksService } from '../tasks/tasks.service';
 
-describe('MonitoredEndpointResolver', () => {
+describe.only('MonitoredEndpointResolver', () => {
+  type MockType<T> = { [P in keyof T]: jest.Mock<{}> };
+
+  const repositoryMockFactory: () => MockType<
+    Partial<Repository<any>>
+  > = jest.fn(() => ({
+    findOne: jest.fn(entity => entity),
+    save: jest.fn(entity => entity),
+  }));
+
+  const mockTaskService = jest.fn(() => ({
+    schedule: jest.fn(p => p),
+  }));
+
+  const user = {
+    id: 'dummy',
+    email: 'dummy',
+    monitoredEndpoint: [],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    username: 'adads',
+    password: 'asdasd',
+  };
+
+  const endpoint = {
+    name: 'name',
+    url: 'google.com',
+    monitoredInterval: 20,
+  };
+
   let resolver: MonitoredEndpointResolver;
+  let entityReposMock: MockType<Repository<MonitoredEndpointEntity>>;
+  let resultReposMock: MockType<Repository<MonitoringResultEntity>>;
+  let taskService: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MonitoredEndpointResolver],
+      providers: [
+        MonitoredEndpointResolver,
+        {
+          provide: TasksService,
+          useClass: mockTaskService,
+        },
+        {
+          provide: getRepositoryToken(MonitoredEndpointEntity),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(MonitoringResultEntity),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     resolver = module.get<MonitoredEndpointResolver>(MonitoredEndpointResolver);
+    entityReposMock = module.get(getRepositoryToken(MonitoredEndpointEntity));
+    resultReposMock = module.get(getRepositoryToken(MonitoringResultEntity));
+    taskService = module.get(TasksService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should create endpoint and schedule task', async () => {
+    entityReposMock.findOne.mockReturnValue(endpoint);
+    await resolver.createMonitoredEndpoint(endpoint, user);
+    expect(entityReposMock.save).toHaveBeenCalledWith({
+      ...endpoint,
+      owner: { id: user.id },
+    });
+    expect(taskService.schedule).toHaveBeenCalledWith(endpoint);
   });
 });
